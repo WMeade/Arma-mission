905cc724c16268c417ee6fd7ff67c55d3a395d73
Wesley Meade <stephenmead13@gmail.com>
2020-09-24 15:31:15 +0100

    Delete CfgFunctions.hpp

--- functions/spawning/CfgFunctions.hpp
+++
@@ -1,6 +0,0 @@
-class Spawning
-{
-	class enemyAssault {file = "functions\spawning\fn_enemyAssault.sqf";};
-	class createMarker {file = "functions\spawning\fn_createMarker.sqf";};
-	class garrisonBuilding {file = "functions\spawning\fn_garrisonBuilding.sqf";};
-};
